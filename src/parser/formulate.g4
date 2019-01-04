8736e689e395679cd11c75385e29dbf1d815a824
sbondaryev <sbondaryev@gmail.com>
2019-01-04 23:23:03 +0100

    remove antlr

--- src/parser/formulate.g4
+++
@@ -1,9 +0,0 @@
-grammar formulate;
-prog: (expr NEWLINE)* ;
-expr: expr ('*'|'/') expr
-    | expr ('+'|'-') expr
-    | INT
-    | '(' expr ')'
-    ;
-NEWLINE : [\r\n]+ ;
-INT     : [0-9]+ ;
--- stories/index.js
+++ stories/index.js
@@ -1,9 +1,7 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
-import { parse } from '../src/parser'
 
 storiesOf('Button', module)
   .add('with text', () => {
-    console.log(parse("100+1\n"))
     return <div>test</div>
   })
