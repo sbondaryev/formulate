72db80846e9cf43d0a61f567a85b7ea47665e9f1
sbondaryev <sbondaryev@gmail.com>
2019-01-18 22:29:11 +0100

    add findClosestRectangle2

--- __tests__/position.test.js
+++ __tests__/position.test.js
@@ -1,8 +1,15 @@
-const {findClosestRectangle} = require('../src/position')
+const {findClosestRectangle, findClosestRectangle2} = require('../src/position')
 
 test('findClosestRectangle', () => {
 
   expect(findClosestRectangle([3, 8, 5, 4], [[2, 9, 6, 3]]))
     .toEqual([ 2, 9, 6, 3 ])
+
+})
+
+test('findClosestRectangle2', () => {
+
+  expect(findClosestRectangle2([3, 8, 5, 4], [[2, 9, 6, 3], [1, 10, 7, 2]]))
+    .toEqual([1, 10, 7, 2])
 
 })
--- src/position.js
+++ src/position.js
@@ -2,13 +2,24 @@ import flow from "lodash/fp/flow"
 import filter from "lodash/fp/filter"
 import sortBy from "lodash/fp/sortBy"
 import first from "lodash/fp/first"
+import get from "lodash/fp/get"
 
 export const findClosestRectangle = (pos, rectangles) => {
   const [t, r, b, l] = pos
   return flow(
     filter(rec => rec != pos),
     filter(([rt, rr, rb, rl]) => t >= rt && r <= rr && b <= rb && l >= rl),
-    sortBy(([r, l, b, t]) => (r - l) * (b - t)),
+    sortBy(([t, r, b, l]) => (r - l) * (b - t)),
     first
+  )(rectangles)
+}
+
+export const findClosestRectangle2 = (pos, rectangles) => {
+  const [t, r, b, l] = pos
+  return flow(
+    filter(rec => rec != pos),
+    filter(([rt, rr, rb, rl]) => t >= rt && r <= rr && b <= rb && l >= rl),
+    sortBy(([t, r, b, l]) => (r - l) * (b - t)),
+    get([1])
   )(rectangles)
 }
