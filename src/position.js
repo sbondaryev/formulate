b3e31800dea98f2701f0861d429cb2f4e2bf1880
sbondaryev <sbondaryev@gmail.com>
2019-01-18 22:39:28 +0100

    factorize enclosingRecs

--- src/position.js
+++ src/position.js
@@ -1,25 +1,20 @@
 import flow from "lodash/fp/flow"
 import filter from "lodash/fp/filter"
 import sortBy from "lodash/fp/sortBy"
-import first from "lodash/fp/first"
 import get from "lodash/fp/get"
 
-export const findClosestRectangle = (pos, rectangles) => {
+const enclosingRectangles = (pos, rectangles) => {
   const [t, r, b, l] = pos
   return flow(
     filter(rec => rec != pos),
     filter(([rt, rr, rb, rl]) => t >= rt && r <= rr && b <= rb && l >= rl),
     sortBy(([t, r, b, l]) => (r - l) * (b - t)),
-    first
   )(rectangles)
 }
 
-export const findClosestRectangle2 = (pos, rectangles) => {
-  const [t, r, b, l] = pos
-  return flow(
-    filter(rec => rec != pos),
-    filter(([rt, rr, rb, rl]) => t >= rt && r <= rr && b <= rb && l >= rl),
-    sortBy(([t, r, b, l]) => (r - l) * (b - t)),
-    get([1])
-  )(rectangles)
-}
+export const findClosestRectangle = (pos, rectangles) =>
+  get([0], enclosingRectangles(pos, rectangles))
+
+
+export const findClosestRectangle2 = (pos, rectangles) =>
+  get([1], enclosingRectangles(pos, rectangles))
