89833518d4c4c0c83bb6baec792d76cd593b8c3e
sbondaryev <sbondaryev@gmail.com>
2019-01-19 00:50:39 +0100

    add isEnclosing

--- __tests__/position.test.js
+++ __tests__/position.test.js
@@ -1,4 +1,8 @@
-const {findClosestRectangle, findClosestRectangle2} = require('../src/position')
+const {
+  findClosestRectangle,
+  findClosestRectangle2,
+  isEnclosing
+} = require('../src/position')
 
 test('findClosestRectangle', () => {
 
@@ -11,5 +15,15 @@ test('findClosestRectangle2', () => {
 
   expect(findClosestRectangle2([3, 8, 5, 4], [[2, 9, 6, 3], [1, 10, 7, 2]]))
     .toEqual([1, 10, 7, 2])
+
+})
+
+test('isEnclosing', () => {
+
+  expect(isEnclosing([2, 9, 6, 3], [[2, 9, 6, 3], [1, 10, 7, 2]]))
+    .toEqual(true)
+
+  expect(isEnclosing([1, 10, 7, 2], [[2, 9, 6, 3], [1, 10, 7, 2]]))
+    .toEqual(false)
 
 })
--- src/position.js
+++ src/position.js
@@ -2,11 +2,13 @@ import flow from "lodash/fp/flow"
 import filter from "lodash/fp/filter"
 import sortBy from "lodash/fp/sortBy"
 import get from "lodash/fp/get"
+import find from "lodash/fp/find"
+import isEqual from "lodash/fp/isEqual"
 
 const enclosingRectangles = (pos, rectangles) => {
   const [t, r, b, l] = pos
   return flow(
-    filter(rec => rec != pos),
+    filter(rec => !isEqual(rec, pos)),
     filter(([rt, rr, rb, rl]) => t >= rt && r <= rr && b <= rb && l >= rl),
     sortBy(([t, r, b, l]) => (r - l) * (b - t)),
   )(rectangles)
@@ -18,3 +20,10 @@ export const findClosestRectangle = (pos, rectangles) =>
 
 export const findClosestRectangle2 = (pos, rectangles) =>
   get([1], enclosingRectangles(pos, rectangles))
+
+export const isEnclosing = (pos, rectangles) => {
+  const [t, r, b, l] = pos
+  const rcs = filter(r => !isEqual(r, pos), rectangles)
+  return !!find(([rt, rr, rb, rl]) =>
+    t >=  rt && r <= rr && b <= rb && l >= rl, rcs)
+}
